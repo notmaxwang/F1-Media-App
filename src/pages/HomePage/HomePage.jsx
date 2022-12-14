141487136ff41dd045daa5f6b0b7205732b81f5d
notmaxwang <maxwang3737@gmail.com>
2022-12-14 00:09:51 -0800

    added randomized background for home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,39 @@
-export default function HomePage({user}) {
-  return(
-    <>
-      <h1>Start exploring, {user.name}!</h1>
-      <h2>Popular Race Threads</h2>
-    </>
-  );
+import {Component} from 'react';
+
+class HomePage extends Component {
+
+
+  componentWillMount() {
+
+    const pictureArray = ['https://i.imgur.com/kr9zzxh.jpg', 
+                          'https://i.imgur.com/obpyB6j.jpg',
+                          'https://i.imgur.com/PqCtiJS.jpg',
+                          'https://i.imgur.com/hEus9ss.jpg'
+                        ];
+    const randomIndex = Math.floor(Math.random() * pictureArray.length);
+    const selectedPicture = pictureArray[randomIndex];
+
+    this.setState({
+      bgStyle: {
+        backgroundImage: `url(${selectedPicture})`,
+        height: "100vh",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }
+    })
+
+  }
+
+  render() {
+    return(
+      <div style={this.state.bgStyle}>
+        <h1>Start exploring!</h1>
+        <h2>Popular Race Threads</h2>
+      </div>
+    );
+  }
+  
 }
+
+export default HomePage;
