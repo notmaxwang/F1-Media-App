c72803cf10659f36b5a6fd0d8b3aec17d5273945
notmaxwang <maxwang3737@gmail.com>
2022-12-15 17:37:07 -0800

    small style changes

--- src/pages/DiscussionBoardPage/DiscussionBoardPage.jsx
+++ src/pages/DiscussionBoardPage/DiscussionBoardPage.jsx
@@ -5,6 +5,7 @@ export default function DiscussionBoardPage() {
   return (
     <div className="DiscussionBoard">
       <h1>DiscussionBoardPage</h1>
+      <h3>Coming Soon!</h3>
     </div>
   );
 }
